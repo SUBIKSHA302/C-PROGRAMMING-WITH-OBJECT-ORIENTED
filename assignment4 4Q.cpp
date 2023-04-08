9f9d38fec08b27c953a7b98bb2b7733387687bb1
SUBIKSHA302 <129165288+SUBIKSHA302@users.noreply.github.com>
2023-04-08 07:09:43 +0530

    Add files via upload

---
+++ assignment4 4Q.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int x = 10;
+   int y = 70;
+
+   int temp = x;
+   x = y;
+   y = temp;
+
+   cout << "X : " << x << "\n";
+   cout << "Y : " << y << "\n";
+
+   return 0;
+}
